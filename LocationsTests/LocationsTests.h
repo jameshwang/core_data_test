b13333c29ca9f6f6cbcc0647f14a194f3a992a8b
James Hwang <james.hwang.usc@gmail.com>
2013-06-27 23:02:32 -0700

    Initial Commit

---
+++ LocationsTests/LocationsTests.h
@@ -0,0 +1,13 @@
+//
+//  LocationsTests.h
+//  LocationsTests
+//
+//  Created by James on 6/27/13.
+//  Copyright (c) 2013 James. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LocationsTests : SenTestCase
+
+@end
---
+++ LocationsTests/LocationsTests.m
@@ -0,0 +1,32 @@
+//
+//  LocationsTests.m
+//  LocationsTests
+//
+//  Created by James on 6/27/13.
+//  Copyright (c) 2013 James. All rights reserved.
+//
+
+#import "LocationsTests.h"
+
+@implementation LocationsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LocationsTests");
+}
+
+@end
